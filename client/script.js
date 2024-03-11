9ec463357fa6879353beb638c160d5ba4eb62f2f
Luther <kanabuluther@gmail.com>
2024-03-11 23:38:10 +0300

    Updated client API endpoint

--- client/script.js
+++ client/script.js
@@ -9,7 +9,7 @@ async function sendMessage() {
 
     try {
         // Make a POST request to the server with user input
-        const response = await fetch('https://botchatserver.netlify.app/api/openai', {
+        const response = await fetch('https://botchatserver.netlify.app/api/chat', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
